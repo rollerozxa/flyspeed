aafea7bc3ca0e5733feb2174ba73360c5652ed05
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-01-22 17:00:12 +0100

    Rewrite mod to persist data in mod storage and give more feedback to the player.

--- init.lua
+++ init.lua
@@ -1,5 +1,18 @@
 local data = {}
 
+local storage = minetest.get_mod_storage()
+
+-- Read flight speed data from mod storage on startup
+minetest.register_on_mods_loaded(function()
+	data = minetest.parse_json(storage:get("data") or "{}")
+end)
+
+-- Function to set speed to 'speed' for 'name'
+function update_speed(name, speed)
+	data[name] = speed
+	storage:set_string("data", minetest.write_json(data))
+end
+
 minetest.register_chatcommand('flyspeed', {
 	params = "<speed>",
 
@@ -8,24 +21,26 @@ minetest.register_chatcommand('flyspeed', {
 	privs = { fly = true },
 
 	func = function(name, param)
-		data[name] = tonumber(param)
+		local speed = tonumber(param) or -1
+		if speed >= 0 then
+			minetest.chat_send_player(name, "Set flight speed to "..param)
+			update_speed(name, speed)
+		else
+			minetest.chat_send_player(name, minetest.colorize("#ff0000", "Invalid speed."))
+		end
 	end,
 })
 
 minetest.register_globalstep(function(dtime)
-	for k, v in pairs(data) do
-		if v ~= nil then
-			player = minetest.get_player_by_name(k)
+	for _,player in pairs(minetest.get_connected_players()) do
+		local speed = data[player:get_player_name()]
+		if speed then
 			if (minetest.get_node({x = player:get_pos().x, y = math.floor(player:get_pos().y) - 1, z = player:get_pos().z}).name == "air"
 			and minetest.get_node({x = player:get_pos().x, y = math.floor(player:get_pos().y), z = player:get_pos().z}).name == "air") and player:get_velocity().y >= 0 then
-				player:set_physics_override({ speed = tonumber(v) })
+				player:set_physics_override({ speed = speed })
 			else
 				player:set_physics_override({ speed = 1 })
 			end
 		end
 	end
-end)
-
-minetest.register_on_leaveplayer(function(player)
-	data[player:get_player_name()] = nil
 end)
