e9265a6fd502efbdaa967e80365872ecddb25ac0
ROllerozxa <rollerozxa@voxelmanip.se>
2024-11-08 21:45:29 +0100

    `minetest` -> `core`

--- init.lua
+++ init.lua
@@ -1,21 +1,21 @@
 local data = {}
 
-local S = minetest.get_translator('flyspeed')
+local S = core.get_translator('flyspeed')
 
-local storage = minetest.get_mod_storage()
+local storage = core.get_mod_storage()
 
 -- Read flight speed data from mod storage on startup
-minetest.register_on_mods_loaded(function()
-	data = minetest.parse_json(storage:get("data") or "{}")
+core.register_on_mods_loaded(function()
+	data = core.parse_json(storage:get("data") or "{}")
 end)
 
 -- Function to set speed to 'speed' for 'name'
 function update_speed(name, speed)
 	data[name] = speed
-	storage:set_string("data", minetest.write_json(data))
+	storage:set_string("data", core.write_json(data))
 end
 
-minetest.register_chatcommand('flyspeed', {
+core.register_chatcommand('flyspeed', {
 	params = "<speed>",
 
 	description = S("Change the flight speed."),
@@ -25,20 +25,20 @@ minetest.register_chatcommand('flyspeed', {
 	func = function(name, param)
 		local speed = tonumber(param) or -1
 		if speed >= 0 then
-			minetest.chat_send_player(name, S("Set flight speed to @1", param))
+			core.chat_send_player(name, S("Set flight speed to @1", param))
 			update_speed(name, speed)
 		else
-			minetest.chat_send_player(name, minetest.colorize("#ff0000", S("Invalid speed.")))
+			core.chat_send_player(name, core.colorize("#ff0000", S("Invalid speed.")))
 		end
 	end,
 })
 
-minetest.register_globalstep(function(dtime)
-	for _,player in pairs(minetest.get_connected_players()) do
+core.register_globalstep(function(dtime)
+	for _,player in pairs(core.get_connected_players()) do
 		local speed = data[player:get_player_name()]
 		if speed then
-			if (minetest.get_node({x = player:get_pos().x, y = math.floor(player:get_pos().y) - 1, z = player:get_pos().z}).name == "air"
-			and minetest.get_node({x = player:get_pos().x, y = math.floor(player:get_pos().y), z = player:get_pos().z}).name == "air") and player:get_velocity().y >= 0 then
+			if (core.get_node({x = player:get_pos().x, y = math.floor(player:get_pos().y) - 1, z = player:get_pos().z}).name == "air"
+			and core.get_node({x = player:get_pos().x, y = math.floor(player:get_pos().y), z = player:get_pos().z}).name == "air") and player:get_velocity().y >= 0 then
 				player:set_physics_override({ speed = speed })
 			else
 				player:set_physics_override({ speed = 1 })
